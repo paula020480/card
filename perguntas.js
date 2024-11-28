f572370025c203aadeee697f1396187dbb177127
paula020480 <0000110443717xsp@al.educacao.sp.gov.br>
2024-11-28 20:09:57 -0300

    pj final

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Musica',
+    'oque é partitura musical?',
+    'Partitura nada mais é do que uma representação escrita de uma determinada música. Assim como qualquer outro sistema de escrita, ela também vem acompanhada de símbolos (notas musicais) que definem cada som da música.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Japão?',
+    'A capital do Japão é Tóquio'
+)
+
+criaCartao(
+    'Biologia',
+    'O que é um aracnídeo?',
+    'São uma subclasse do filo artrópode, que é o maior filo do reino animal, com cerca de 80% dos seres vivos. Esse grupo é representado por animais como aranhas, ácaros, escorpiões e entre outras mais de 60 mil espécies.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz escola em Inglês?',
+    'ESCOLA em ingles é SCHOOL '
+)
